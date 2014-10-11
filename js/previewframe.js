0cf32fd0554584030274cf03269f4f2cec6d0241
Wouter Verweirder <wouter.verweirder@gmail.com>
2014-10-11 14:11:20 +0200

    escape html for console logging

--- js/previewframe.js
+++ js/previewframe.js
@@ -1,7 +1,7 @@
 console.log = function(){
 	args = [];
 	[].forEach.call(arguments, function(argument){
-		args.push(argument.toString());
+		args.push(htmlEscape(argument));
 	});
 	parent.postMessage({type: 'console.log', arguments: args}, '*');
 }
@@ -9,7 +9,17 @@ console.log = function(){
 console.error = function(){
 	args = [];
 	[].forEach.call(arguments, function(argument){
-		args.push(argument.toString());
+		args.push(htmlEscape(argument));
 	});
 	parent.postMessage({type: 'console.error', arguments: args}, '*');
+}
+
+//http://stackoverflow.com/questions/1219860/html-encoding-in-javascript-jquery
+function htmlEscape(str) {
+  return String(str)
+          .replace(/&/g, '&amp;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#39;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;');
 }
